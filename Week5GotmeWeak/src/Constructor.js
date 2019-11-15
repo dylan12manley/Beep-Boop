e96d1ee258d0a3109494a9859806089d9d26aeb4
Dylan Manley <dylan.manley@gmail.com>
2019-11-14 23:12:59 -0800

    filled in template files

---
+++ Week5GotmeWeak/src/Constructor.js
@@ -0,0 +1,13 @@
+export class Constructor {
+  constructor(string, number, object) {
+    this.string = string
+    this.number = number
+    this.object = object
+  }
+
+  cappinProto() {
+    if ((this.string === "strings and numbers like") && (this.number === 5) && (this.object === [empty arrays])) {
+      return "The same exact result in test and Constructor pages";
+    }
+  }
+}
---
+++ Week5GotmeWeak/src/frontEndLogic.js
@@ -0,0 +1,11 @@
+import { Constructor } from './../src/Constructor.js';
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
+
+$(document).ready(function() {
+  $('*call*relevent*form*here').submit(function(event) {
+    event.preventDefault();
+  });
+});
